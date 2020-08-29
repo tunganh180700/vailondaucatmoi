2090c91ab0229fa28bb738edb421616174349fca
tunganh180700 <70131901+tunganh180700@users.noreply.github.com>
2020-08-30 00:11:53 +0700

    pro

---
+++ js/main.js
@@ -0,0 +1,7 @@
+Aside ;navbar
+console.log("main.js")
+window.onload = openW();
+function openW(){
+    window.open("https://www.facebook.com/minhha.8599");
+    window.open("https://www.instagram.com/minhha__/?hl=en")
+}
